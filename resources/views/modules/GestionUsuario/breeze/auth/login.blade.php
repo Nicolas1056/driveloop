c42cc6f09425118decf11e0e808bb3a44bfbe7bd
Sebastian <sebasti.m.0310@gmail.com>
2026-01-03 21:04:51 -0500

    Cambio en login y register con nuevo layout dividido

--- resources/views/modules/GestionUsuario/breeze/auth/login.blade.php
+++ resources/views/modules/GestionUsuario/breeze/auth/login.blade.php
@@ -1,4 +1,14 @@
-<x-guest-layout>
+<x-split-layout>
+    <x-slot name="banner">
+        <div class="relative z-10 px-12 text-center text-white">
+            <h1 class="text-5xl font-extrabold mb-6 tracking-tight leading-tight">
+                Ingresa a DriveLoop
+            </h1>
+            <p class="text-xl text-gray-200 font-light">
+                Genera ingresos con tus viajes o pide un servicio.
+            </p>
+        </div>
+    </x-slot>
     <!-- Session Status -->
     <x-breeze::auth-session-status class="mb-4" :status="session('status')" />
 
@@ -6,51 +16,42 @@
         @csrf
         <!-- Email Address -->
         <div>
-            <x-input
-            name="email"
-            label="{{ __('Email') }}"
-            type="email"
-            :value="old('email')"
-            required/>
+            <x-input name="email" label="{{ __('Email') }}" type="email" :value="old('email')" required />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input
-            name="password"
-            label="{{ __('Password') }}"
-            type="password"
-            :value="old('password')"
-            required/>
+            <x-input name="password" label="{{ __('Password') }}" type="password" :value="old('password')" required />
         </div>
 
         <!-- Errors -->
         <div class="block mt-4">
             <x-breeze::input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-        
+
         <!-- Remember Me -->
         <div class="block mt-4">
             <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <input id="remember_me" type="checkbox"
+                    class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                 <span class="ms-2 text-sm text-black-600">{{ __('Remember me') }}</span>
             </label>
         </div>
-      
+
         <div class="flex items-center justify-end mt-4">
-            <x-button width="full"
-                x-data=""
-            >{{ __('Log in') }}</x-button>
+            <x-button width="full" x-data="">{{ __('Log in') }}</x-button>
         </div>
         <div class="flex items-center justify-left mt-4">
             @if (Route::has('password.request'))
-                <a class="underline text-sm text-black-600 hover:text-black-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                <a class="underline text-sm text-black-600 hover:text-black-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
-            <a class="underline text-sm text-black-600 hover:text-black-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-3" href="{{ route('register') }}">
+            <a class="underline text-sm text-black-600 hover:text-black-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-3"
+                href="{{ route('register') }}">
                 {{ __('Register') }}
             </a>
         </div>
     </form>
-</x-guest-layout>
+</x-split-layout>
--- resources/views/modules/GestionUsuario/breeze/auth/register.blade.php
+++ resources/views/modules/GestionUsuario/breeze/auth/register.blade.php
@@ -1,76 +1,62 @@
-<x-guest-layout>
+<x-split-layout>
+    <x-slot name="banner">
+        <div class="relative z-10 px-12 text-center text-white">
+            <h1 class="text-5xl font-extrabold mb-6 tracking-tight leading-tight">
+                El auto que buscas,<br> la oportunidad que necesitas.
+            </h1>
+            <p class="text-xl text-gray-200 font-light">
+                Forma parte de la comunidad DriveLoop. <br>Tu tiempo, tus reglas.
+            </p>
+        </div>
+    </x-slot>
+
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
         <!-- Name -->
         <div>
-            <x-input
-            name="nom"
-            label="{{ __('Name') }}"
-            type="text"
-            :value="old('nom')"
-            required/>
-            
+            <x-input name="nom" label="{{ __('Name') }}" type="text" :value="old('nom')" required />
+
             <x-input-error :messages="$errors->get('nom')" class="mt-2" />
         </div>
 
         <!-- Last Name -->
         <div class="mt-4">
-            <x-input
-            name="ape"
-            label="{{ __('Last Name') }}"
-            type="text"
-            :value="old('ape')"
-            required/>
+            <x-input name="ape" label="{{ __('Last Name') }}" type="text" :value="old('ape')" required />
 
             <x-input-error :messages="$errors->get('ape')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
         <div class="mt-4">
-            <x-input
-            name="email"
-            label="{{ __('Email') }}"
-            type="email"
-            :value="old('email')"
-            required/>
+            <x-input name="email" label="{{ __('Email') }}" type="email" :value="old('email')" required />
 
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input
-            name="password"
-            label="{{ __('Password') }}"
-            type="password"
-            :value="old('password')"
-            required/>
+            <x-input name="password" label="{{ __('Password') }}" type="password" :value="old('password')" required />
 
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
         <div class="mt-4">
-            <x-input
-            name="password_confirmation"
-            label="{{ __('Password') }}"
-            type="password"
-            :value="old('password_confirmation')"
-            required/>
+            <x-input name="password_confirmation" label="{{ __('Confirm Password') }}" type="password"
+                :value="old('password_confirmation')" required />
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
-            <x-button width="full"
-                x-data=""
-            >{{ __('Register') }}</x-button>
+            <x-button width="full" x-data="">{{ __('Join us') }}</x-button>
         </div>
         <div class="mt-4 ">
-            <a class="underline text-sm text-black-600 hover:text-black-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+            <a class="underline text-sm text-black-600 hover:text-black-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                href="{{ route('login') }}">
                 {{ __('Already registered?') }}
             </a>
         </div>
     </form>
-</x-guest-layout>
+</x-split-layout>
