401c3d19f37318a59a98268ba755f11b23aa0add
DriveLoop <driveloop7@gmail.com>
2026-01-09 15:43:52 -0500

    Update modal.blade.php

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -1,24 +1,8 @@
 @props([
     'name',
     'title' => '',
-    'show' => false,
-    'maxWidth' => '2xl'
+    'show' => false
 ])
-
-@php
-$maxWidth = [
-    'sm' => 'sm:max-w-sm',
-    'md' => 'sm:max-w-md',
-    'lg' => 'sm:max-w-lg',
-    'xl' => 'sm:max-w-xl',
-    '2xl' => 'sm:max-w-2xl',
-    '3xl' => 'sm:max-w-3xl',
-    '4xl' => 'sm:max-w-4xl',
-    '5xl' => 'sm:max-w-5xl',
-    '6xl' => 'sm:max-w-6xl',
-    '7xl' => 'sm:max-w-7xl',
-][$maxWidth];
-@endphp
 
 <div
     x-data="{show: @js($show), params: {}}"
@@ -51,19 +35,19 @@ $maxWidth = [
     
     <div
         x-show="show"
-        class="mb-6 bg-transparent overflow-hidden transform transition-all mx-auto {{ $maxWidth }}"
         x-transition:enter="ease-out duration-300"
         x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
         x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
         x-transition:leave="ease-in duration-200"
         x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+        x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        {{ $attributes->merge([
+            'class' => 'mb-6 bg-transparent overflow-hidden transform transition-all sm:max-w-md md:max-w-xl mx-auto']) }}
+        >
         
-        @if($title)
         <div class="bg-gradient-to-r from-dl to-dl-two -ml-8 px-6 py-3 text-white text-2xl font-bold w-[70%] xl:w-[60%] skew-x-35 uppercase">
             <span class="-skew-x-35 block ml-8">{{ $title }}</span>
         </div>
-        @endif
             
         <div x-data="{ active: null }"
             class="bg-white xl:rounded-r-lg xl:rounded-bl-lg p-6">
