3161ebac1e3a5b84b1adfb7a0c4160b9a4235ec3
Sebastian <sebasti.m.0310@gmail.com>
2026-01-16 17:11:26 -0500

    Correccion de ruta inicial (recibiendo get y post)

--- app/Modules/BusquedaReserva/routes.php
+++ app/Modules/BusquedaReserva/routes.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Modules\BusquedaReserva\Controllers\BusquedaReservaController;
 
 Route::prefix('busqueda-reserva')->group(function () {
-    Route::post('/', [BusquedaReservaController::class, 'index'])->name('busqueda.reserva');
+    Route::match(['get', 'post'], '/', [BusquedaReservaController::class, 'index'])->name('busqueda.reserva');
 });
